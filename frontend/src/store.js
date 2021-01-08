3942b894fc51f596b705f6fb883d08922bfcfaad
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-08 13:23:01 +0100

    bring redux state to home component with useDispatch and useSelector api

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer } from './reducers/productReducers'
+
 const reducer = combineReducers({ productList: productListReducer })
 
 const initialState = {}
