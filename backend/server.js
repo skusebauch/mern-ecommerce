ac3c1a61381fa0e6dcdf3916a5492caf70d2de06
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-08 11:37:42 +0100

    set up error middleware for custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productsRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productsRoutes)
+
+// middleware to handle custom error message - wrong url
+app.use(notFound)
+// middleware to handle custom error message - wrong product id
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
