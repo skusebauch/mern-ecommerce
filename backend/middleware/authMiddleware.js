8693d3a4e9da2d84542bc6168980fda0ce428a09
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-10 19:36:24 +0100

    set up user registration & password encryption

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,6 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1]
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
       // assign user to req.user to use in any protected route you want
       req.user = await User.findById(decoded.id).select('-password')
 
