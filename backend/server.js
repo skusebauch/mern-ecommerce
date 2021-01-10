330d802910554587f7f28c52ff6260ba4f5a727a
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-10 15:15:53 +0100

    set up user auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,23 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productsRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
 connectDB()
+
 const app = express()
+
+// body parser to allow sending body - user req.body at usersController.js
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
 app.use('/api/products', productsRoutes)
+app.use('/api/users', userRoutes)
 
 // middleware to handle custom error message - wrong url
 app.use(notFound)
