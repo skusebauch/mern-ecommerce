b1e04dc71535730a375e80406d6834c63830c48e
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-11 16:08:01 +0100

    add login reducer and action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import * as types from '../constants/userConstants'
+
+export const login = (email, password) => async dispatch => {
+  try {
+    dispatch({
+      type: types.USER_LOGIN_REQUEST,
+    })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    )
+    // data = get back from userController.js res.json({})
+    dispatch({
+      type: types.USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: types.USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    })
+  }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,11 +6,13 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
 // localStorage set at "./actions/cartActions.js" #addToCart
@@ -18,9 +20,16 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage,
   },
 }
 
