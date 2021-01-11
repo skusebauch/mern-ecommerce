69eedaee880b08e09058a2b52c13d98ca93d6561
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-11 17:27:30 +0100

    add register

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -41,3 +41,42 @@ export const logout = () => dispatch => {
     type: types.USER_LOGOUT,
   })
 }
+
+export const register = (name, email, password) => async dispatch => {
+  try {
+    dispatch({
+      type: types.USER_REGISTER_REQUEST,
+    })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    )
+    // data = get back from userController.js res.json({})
+    dispatch({
+      type: types.USER_REGISTER_SUCCESS,
+      payload: data,
+    })
+    // after register automatically logged in
+    dispatch({
+      type: types.USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: types.USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    })
+  }
+}
