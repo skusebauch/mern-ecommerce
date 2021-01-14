19fb8621b06884dd3e0ef7253942243700a3e29a
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-14 11:55:00 +0100

    add toPaid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,11 +2,16 @@ import express from 'express'
 
 const router = express.Router()
 
-import { addOrderItems, getOrderById } from '../controllers/orderController.js'
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js'
 import protect from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
 // make sure to make :id at the bottom same as rails
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 export default router
