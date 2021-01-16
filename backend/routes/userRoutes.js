2f4fa7c78b20712d523ffbd80c8ba158eef3ab63
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-16 16:59:37 +0100

    add remove route and controller backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,7 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
+  deleteUser,
 } from '../controllers/userController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
@@ -18,5 +19,5 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
-
+router.route('/:id').delete(protect, isAdmin, deleteUser)
 export default router
