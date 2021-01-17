73a40325907de01f40a63b47a585e3a8f6925736
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-01-17 14:21:19 +0100

    set up controller and routes for delete a product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,15 @@ import express from 'express'
 
 const router = express.Router()
 
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
+
 import {
+  deleteProduct,
   getProductById,
   getProducts,
 } from '../controllers/productController.js'
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct)
 
 export default router
